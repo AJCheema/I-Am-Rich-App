5cd6d183d760d2ac33c1f08729be7f044e0d36f4
Ali Cheema <cheema36912@gmail.com>
2020-07-08 04:27:49 +0500

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ali Cheema on 08/07/2020.
+//  Copyright © 2020 Ali Cheema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
